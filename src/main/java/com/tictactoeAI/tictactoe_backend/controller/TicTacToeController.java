b7d0e995f2997996a58860cfac340919d51624cc
FriedricNietzsche <krisviraujla@gmail.com>
2025-05-10 17:08:34 -0400

    Change TicTacToe Controller for allow for all origins after deployment of website on vercel

--- src/main/java/com/tictactoeAI/tictactoe_backend/controller/TicTacToeController.java
+++ src/main/java/com/tictactoeAI/tictactoe_backend/controller/TicTacToeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/game")
 public class TicTacToeController {
